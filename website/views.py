1a402f7446f1b51498dd9cb744525ee09e3c7df3
tharindu darshana <tdmw2k19@gmail.com>
2023-04-14 20:06:15 +0530

    Delete records

--- website/views.py
+++ website/views.py
@@ -53,3 +53,13 @@ def customer_record(request,pk):
       else:
            messages.success(request, "You Must be logged in to view that page.") 
            return redirect('home')
+      
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Delete Successfully..!")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must be logged in to view that page.") 
+        return redirect('home')
